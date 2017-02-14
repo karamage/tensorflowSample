5170b19efd2dabb2ca978c7deba9698dcbad31dd
Masaaki Kakimoto <karamage.umauma@gmail.com>
2017-02-15 08:10:31 +0900

    ロジスティック回帰

--- ch02/main.py
+++ ch02/main.py
@@ -25,10 +25,30 @@ train_set = df.reindex(permutation(df.index)).reset_index(drop=True)
 print(train_set)
 
 train_x = train_set[['x1', 'x2']].as_matrix()
-train_y = train_set['t'].as_matrix().reshape([len(train_set), 1])
+train_t = train_set['t'].as_matrix().reshape([len(train_set), 1])
 
 x = tf.placeholder(tf.float32, [None, 2])
 w = tf.Variable(tf.zeros([2, 1]))
 w0 = tf.Variable(tf.zeros([1]))
 f = tf.matmul(x, w) + w0
 p = tf.sigmoid(f)
+
+t = tf.placeholder(tf.float32, [None, 1])
+loss = -tf.reduce_sum(t*tf.log(p) + (1-t)*tf.log(1-p))
+train_step = tf.train.AdamOptimizer().minimize(loss)
+
+correct_prediction = tf.equal(tf.sign(p-0.5), tf.sign(t-0.5))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.Session()
+sess.run(tf.initialize_all_variables())
+
+i = 0
+for _ in range(20000):
+    i += 1
+    sess.run(train_step, feed_dict={x:train_x, t:train_t})
+    if i % 2000 == 0:
+        loss_val, acc_val = sess.run(
+            [loss, accuracy], feed_dict={x:train_x, t:train_t})
+        print ('Step: %d, Loss: %f, Accuracy: %f' % (i, loss_val, acc_val))
+
