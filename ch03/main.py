fff6c479cb6597bc001b2743ce05acbfb4cc761c
Masaaki Kakimoto <karamage.umauma@gmail.com>
2017-02-20 08:05:58 +0900

    最尤推定法

--- ch03/main.py
+++ ch03/main.py
@@ -26,3 +26,34 @@ train_set = df.reindex(permutation(df.index)).reset_index(drop=True)
 train_x = train_set[['x1','x2']].as_matrix()
 train_t = train_set['t'].as_matrix().reshape([len(train_set),1])
 
+num_units = 2
+mult = train_x.flatten().mean()
+
+x = tf.placeholder(tf.float32, [None, 2])
+
+w1 = tf.Variable(tf.truncated_normal([2, num_units]))
+b1 = tf.Variable(tf.zeros([num_units]))
+hidden1 = tf.nn.tanh(tf.matmul(x, w1) + b1*mult)
+
+w0 = tf.Variable(tf.zeros([num_units, 1]))
+b0 = tf.Variable(tf.zeros([1]))
+p  = tf.nn.sigmoid(tf.matmul(hidden1, w0) + b0*mult)
+
+t = tf.placeholder(tf.float32, [None, 1])
+loss = -tf.reduce_sum(t*tf.log(p) + (1-t)*tf.log(1-p))
+train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
+correct_prediction = tf.equal(tf.sign(p-0.5), tf.sign(t-0.5))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.InteractiveSession()
+sess.run(tf.initialize_all_variables())
+
+i = 0
+
+for _ in range(1000):
+    i += 1
+    sess.run(train_step, feed_dict={x:train_x, t:train_t})
+    if i % 100 == 0:
+        loss_val, acc_val = sess.run(
+            [loss, accuracy], feed_dict={x:train_x, t:train_t})
+        print('Step: %d, Loss: %f, Accuracy: %f' % (i, loss_val, acc_val))
