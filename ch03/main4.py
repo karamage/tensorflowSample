07d5084c522e6c32b2f148a247e59876e9de52e6
Masaaki Kakimoto <karamage.umauma@gmail.com>
2017-02-26 10:48:03 +0900

    畳み込みフィルター

--- ch03/main4.py
+++ ch03/main4.py
@@ -16,8 +16,8 @@ def generate_datablock(n, mu, var, t):
     return df
 
 df0 = generate_datablock(30, [-7,-7], 18, 1)
-df1 = generate_datablock(30, [-7,-7], 18, 0)
-df2 = generate_datablock(30, [7,-7], 18, 1)
+df1 = generate_datablock(30, [-7,7], 18, 0)
+df2 = generate_datablock(30, [7,-7], 18, 0)
 df3 = generate_datablock(30, [7,7], 18, 1)
 
 df = pd.concat([df0, df1, df2, df3], ignore_index=True)
@@ -31,3 +31,52 @@ num_units2 = 2
 
 x  = tf.placeholder(tf.float32, [None, 2])
 w1 = tf.Variable(tf.truncated_normal([2, num_units1]))
+b1 = tf.Variable(tf.zeros(num_units1))
+hidden1 = tf.nn.tanh(tf.matmul(x, w1) + b1)
+
+w2 = tf.Variable(tf.truncated_normal([num_units1, num_units2]))
+b2 = tf.Variable(tf.zeros([num_units2]))
+hidden2 = tf.nn.tanh(tf.matmul(hidden1, w2) + b2)
+
+w0 = tf.Variable(tf.zeros([num_units2, 1]))
+b0 = tf.Variable(tf.zeros([1]))
+p = tf.nn.sigmoid(tf.matmul(hidden2, w0) + b0)
+
+t = tf.placeholder(tf.float32, [None, 1])
+loss = -tf.reduce_sum(t*tf.log(p) + (1-t)*tf.log(1-p))
+train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
+correct_prediction = tf.equal(tf.sign(p-0.5), tf.sign(t-0.5))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+sess = tf.InteractiveSession()
+sess.run(tf.initialize_all_variables())
+
+i = 0
+for _ in range(2000):
+    i += 1
+    sess.run(train_step, feed_dict={x:train_x, t:train_t})
+    if i % 100 == 0:
+        loss_val, acc_val = sess.run(
+            [loss, accuracy], feed_dict={x:train_x, t:train_t})
+        print('Step: %d, Loss: %f, Accuracy: %f' % (i, loss_val, acc_val))
+
+train_set1 = train_set[train_set['t']==1]
+train_set2 = train_set[train_set['t']==0]
+
+fig = plt.figure(figsize=(6,6))
+subplot = fig.add_subplot(1,1,1)
+subplot.set_ylim([-15,15])
+subplot.set_xlim([-15,15])
+subplot.scatter(train_set1.x1, train_set1.x2, marker='x')
+subplot.scatter(train_set2.x1, train_set2.x2, marker='o')
+
+locations = []
+for x2 in np.linspace(-15, 15, 100):
+    for x1 in np.linspace(-15,15,100):
+        locations.append((x1,x2))
+
+p_vals = sess.run(p, feed_dict={x:locations})
+p_vals = p_vals.reshape((100,100))
+subplot.imshow(p_vals, origin='lower', extent=(-15,15,-15,15),
+               cmap=plt.cm.gray_r, alpha=0.5)
+plt.show()
---
+++ ch04/main.py
@@ -0,0 +1,64 @@
+# coding: UTF-8
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import cPickle as pickle
+
+with open('ORENIST.data', 'rb') as file:
+    images, labels = pickle.load(file)
+
+fig = plt.figure(figsize=(10,5))
+for i in range(40):
+    subplot = fig.add_subplot(4, 10, i+1)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.set_title('%d' % np.argmax(labels[i]))
+    subplot.imshow(images[i].reshape(28,28), vmin=0, vmax=1,
+                   cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
+
+def edge_filter():
+    filter0 = np.array(
+        [[ 2, 1, 0, -1, -2],
+         [ 3, 2, 0, -2, -3],
+         [ 4, 3, 0, -3, -4],
+         [ 3, 2, 0, -2, -3],
+         [ 2, 1, 0, -1, -2]]) / 23.0
+    filter1 = np.array(
+        [[2, 3, 4, 3, 2],
+         [1, 2, 3, 2, 1],
+         [0, 0, 0, 0, 0],
+         [-1,-2,-3,-2,-1],
+         [-2,-3,-4,-3,-2]]) / 23.0
+
+    filter_array = np.zeros([5,5,1,2])
+    filter_array[:,:,0,0] = filter0
+    filter_array[:,:,0,1] = filter1
+
+    return tf.constant(filter_array, dtype=tf.float32)
+
+x = tf.placeholder(tf.float32, [None, 784])
+x_image = tf.reshape(x, [-1,28,28,1])
+
+W_conv = edge_filter()
+h_conv = tf.abs(tf.nn.conv2d(x_image, W_conv,
+                             strides=[1,1,1,1], padding='SAME'))
+h_conv_cutoff = tf.nn.relu(h_conv-0.2)
+
+h_pool = tf.nn.max_pool(h_conv_cutoff, ksize=[1,2,2,1],
+                        strides=[1,2,2,1], padding='SAME')
+
+sess = tf.InteractiveSession()
+sess.run(tf.initialize_all_variables())
+
+filter_vals, conv_vals = sess.run([W_conv, h_conv_cutoff],
+                                  feed_dict={x:images[:9]})
+
+fig = plt.figure(figsize=(10,3))
+
+for i in range(2):
+    subplot = fig.add_subplot(3, 10, 10*(i+1)+1)
+    subplot.set_xticks([])
+    subplot.set_yticks([])
+    subplot.imshow(filter_vals[:,:,0,i],
+                   cmap=plt.cm.gray_r, interpolation='nearest')
